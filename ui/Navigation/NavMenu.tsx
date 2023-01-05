86412f95d4409ab0a3dd069cbeaa9f6a621713f3
Vahizan <vahizanv@gmail.com>
2023-01-05 23:11:54 +0000

    add white background on hover

--- ui/Navigation/NavMenu.tsx
+++ ui/Navigation/NavMenu.tsx
@@ -1,25 +1,38 @@
 import styles from "./Navigation.module.scss";
-import {NavMenuType} from "./types";
+import { NavMenuType } from "./types";
 import NavItem from "./NavItem";
 
-
-export default function NavMenu({root, children}: NavMenuType ) {
-    if(!children && !root){
-        return;
-    }
-    if(!children && root){
-        return <NavItem title={root.title} link={root.link} icon={root?.icon} imageAlt={root?.imageAlt}/>
-    }
+export default function NavMenu({ root, children }: NavMenuType) {
+  if (!children && !root) {
+    return;
+  }
+  if (!children && root) {
     return (
-        <div className={styles.navMenu}>
-            <div className={styles.navMenu__root}>
-                <NavItem key={`${root.title}_root`} isRoot title={root.title} link={root.link} icon={root?.icon} imageAlt={root?.imageAlt}/>
-                <ul className={styles.navMenu__root__children}>
-                    {children.map((child) => {
-                        return NavMenu(child);
-                    })}
-                </ul>
-            </div>
-        </div>
+      <NavItem
+        title={root.title}
+        link={root.link}
+        icon={root?.icon}
+        imageAlt={root?.imageAlt}
+      />
     );
+  }
+  return (
+    <div className={styles.navMenu}>
+      <div className={styles.navMenu__root}>
+        <NavItem
+          key={`${root.title}_root`}
+          isRoot
+          title={root.title}
+          link={root.link}
+          icon={root?.icon}
+          imageAlt={root?.imageAlt}
+        />
+        <ul className={styles.navMenu__root__children}>
+          {children.map((child) => {
+            return NavMenu(child);
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
