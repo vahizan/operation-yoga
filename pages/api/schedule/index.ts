c428efdb9e2994ba8c91def76cf04203f245788e
Vahizan <vahizanv@gmail.com>
2023-05-12 01:10:34 +0100

    update mongodb env variable key

--- pages/api/schedule/index.ts
+++ pages/api/schedule/index.ts
@@ -15,7 +15,7 @@ export default async function handler(
   }
 
   const mongoConnector = new MongooseDatabaseConnection(
-    process.env.MONGO_DB_URI || "",
+    process.env.MONGODB_URI || "",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
