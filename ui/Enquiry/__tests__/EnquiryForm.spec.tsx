601f71d7e8f461f89c415a637ee78f346b6e8234
Vahizan <vahizanv@gmail.com>
2023-07-09 11:15:23 +0100

    fix enquiry form tests

--- ui/Enquiry/__tests__/EnquiryForm.spec.tsx
+++ ui/Enquiry/__tests__/EnquiryForm.spec.tsx
@@ -1,78 +1,112 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import EnquiryForm from "../EnquiryForm";
-import axios from "axios";
-import useApi from "../../../hooks/useApi/useApi";
-import Email from "../../../hooks/interfaces/Email";
+import { sendEmail } from "../../../hooks/api";
 
-jest.mock("../../../hooks/useApi/useApi");
+jest.mock("../../../hooks/api", () => ({
+  sendEmail: jest.fn(),
+}));
 
 describe("EnquiryForm", () => {
-  it("should render the form fields", () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  test("renders form fields", () => {
     render(<EnquiryForm />);
 
-    const emailInput = screen.getByPlaceholderText("Email");
-    const messageInput = screen.getByPlaceholderText("Enter your message");
-    const submitButton = screen.getByText("Submit");
-
-    expect(emailInput).toBeInTheDocument();
-    expect(messageInput).toBeInTheDocument();
-    expect(submitButton).toBeInTheDocument();
+    expect(screen.getByLabelText("Name")).toBeInTheDocument();
+    expect(screen.getByLabelText("Email")).toBeInTheDocument();
+    expect(screen.getByLabelText("Subject")).toBeInTheDocument();
+    expect(screen.getByLabelText("Message")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
   });
 
-  it("should submit the form with valid email and message", async () => {
-    (useApi as jest.Mock).mockReturnValue({
-      data: "success",
-      apiLoading: false,
-      apiCallErrorMessage: undefined,
-    });
-    // jest
-    //   .spyOn(axios, "post")
-    //   .mockResolvedValue({ status: 200, data: "all good" });
-
+  test("displays error messages for required fields", () => {
     render(<EnquiryForm />);
 
-    const emailInput = screen.getByPlaceholderText("Email");
-    const messageInput = screen.getByPlaceholderText("Enter your message");
-    const submitButton = screen.getByText("Submit");
+    fireEvent.click(screen.getByRole("button", { name: "Submit" }));
 
-    // Enter valid email and message
-    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
-    fireEvent.change(messageInput, { target: { value: "Test message" } });
+    expect(screen.getByText("Name is required")).toBeInTheDocument();
+    expect(screen.getByText("Email is required")).toBeInTheDocument();
+    expect(screen.getByText("Subject is required")).toBeInTheDocument();
+    expect(screen.getByText("Message is required")).toBeInTheDocument();
+  });
 
-    // Submit the form
+  test("displays error message for invalid email", async () => {
+    render(<EnquiryForm />);
+
+    const emailField = screen.getByLabelText("Email");
+    fireEvent.change(emailField, {
+      target: { value: "invalid-email" },
+    });
+    fireEvent.blur(emailField);
+    const submitButton = screen.getByRole("button", { name: "Submit" });
     fireEvent.click(submitButton);
 
     await waitFor(() => {
-      expect(screen.getByText("Enquiry sent successfully")).toBeInTheDocument();
-    });
-  });
-
-  it("should display an error message for invalid email", async () => {
-    (useApi as jest.Mock).mockReturnValue({
-      data: "success",
-      apiLoading: false,
-      apiCallErrorMessage: undefined,
-    });
-    render(<EnquiryForm />);
-
-    const emailInput = screen.getByPlaceholderText("Email");
-    const messageInput = screen.getByPlaceholderText("Enter your message");
-    const submitButton = screen.getByText("Submit");
-
-    // Enter invalid email and valid message
-    fireEvent.change(emailInput, { target: { value: "invalid-email" } });
-    fireEvent.change(messageInput, { target: { value: "Test message" } });
-
-    // Submit the form
-    fireEvent.click(submitButton);
-
-    // Wait for the response
-    await waitFor(() => {
-      expect(
-        // screen.getByText("Failed to send your enquiry. Please try again")
-        screen.getByText("Email is invalid")
-      ).toBeInTheDocument();
       expect(submitButton).toBeDisabled();
+      expect(screen.getByText("Email is invalid")).toBeInTheDocument();
     });
+  });
+
+  test("should not display error messages before submit clicked", async () => {
+    render(<EnquiryForm />);
+
+    await waitFor(() => {
+      expect(() => screen.getByText("Email is invalid")).toThrowError();
+      expect(() => screen.getByText("Message is required")).toThrowError();
+      expect(() => screen.getByText("Subject is required")).toThrowError();
+    });
+  });
+
+  test("displays success message on successful form submission", async () => {
+    (sendEmail as jest.Mock).mockResolvedValue("COOL");
+
+    render(<EnquiryForm />);
+
+    fireEvent.change(screen.getByLabelText("Name"), {
+      target: { value: "John Doe" },
+    });
+    fireEvent.change(screen.getByLabelText("Email"), {
+      target: { value: "johndoe@example.com" },
+    });
+    fireEvent.change(screen.getByLabelText("Subject"), {
+      target: { value: "Test Subject" },
+    });
+    fireEvent.change(screen.getByLabelText("Message"), {
+      target: { value: "Test Message" },
+    });
+    fireEvent.click(screen.getByRole("button", { name: "Submit" }));
+
+    await waitFor(() =>
+      expect(screen.getByText("Enquiry sent successfully")).toBeInTheDocument()
+    );
+  });
+
+  test("displays error message on failed form submission", async () => {
+    (sendEmail as jest.Mock).mockRejectedValueOnce(
+      new Error("Submission Error")
+    );
+
+    render(<EnquiryForm />);
+
+    fireEvent.change(screen.getByLabelText("Name"), {
+      target: { value: "John Doe" },
+    });
+    fireEvent.change(screen.getByLabelText("Email"), {
+      target: { value: "johndoe@example.com" },
+    });
+    fireEvent.change(screen.getByLabelText("Subject"), {
+      target: { value: "Test Subject" },
+    });
+    fireEvent.change(screen.getByLabelText("Message"), {
+      target: { value: "Test Message" },
+    });
+    fireEvent.click(screen.getByRole("button", { name: "Submit" }));
+
+    await waitFor(() =>
+      expect(
+        screen.getByText("Failed to send your enquiry. Please try again")
+      ).toBeInTheDocument()
+    );
   });
 });
