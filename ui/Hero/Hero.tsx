3adbc7f7feff23400b8adc31a3ed79875e844c43
Vahizan <vahizanv@gmail.com>
2023-07-13 00:48:47 +0100

    detect network slowness and switch to image

--- ui/Hero/Hero.tsx
+++ ui/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import styles from "../Card.module.scss";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 interface Props {
   videoUrl?: string;
@@ -8,24 +9,39 @@ interface Props {
   cta?: string;
   ctaText?: string;
 }
+
 export default function Hero({ videoUrl, content, cta, ctaText }: Props) {
+  const [shouldLoadImage, setShouldLoadImage] = useState(true);
+
+  useEffect(() => {
+    const networkInformation = navigator.connection as NetworkInformation;
+    console.log("networkInformation", networkInformation);
+    const effectiveType: string = networkInformation?.effectiveType;
+    console.log("EFFECTIVE TYPE", effectiveType);
+    if (effectiveType === "4g") {
+      setShouldLoadImage(false);
+    }
+  }, []);
+
   return (
     <div className={styles.hero}>
-      {videoUrl && (
+      {!shouldLoadImage && videoUrl && (
         <div className={styles.hero__videoWrap}>
           <video controls={false} autoPlay muted loop>
             <source src={videoUrl} type="video/mp4" />
           </video>
         </div>
       )}
-      <div className={styles.hero__placeholderImage}>
-        <Image
-          alt={"picture of the river ganges"}
-          src={"/Ganga.jpeg"}
-          width={500}
-          height={500}
-        />
-      </div>
+      {shouldLoadImage && (
+        <div className={styles.hero__placeholderImage}>
+          <Image
+            alt={"picture of the river ganges"}
+            src={"/Ganga.jpeg"}
+            width={500}
+            height={500}
+          />
+        </div>
+      )}
       <header className={styles.hero__content}>
         <h1>{content}</h1>
         {cta && ctaText && (
