8b8bd0a4d8a01d4abded15fb85040999a2fab7ab
Vahizan <vahizanv@gmail.com>
2024-03-05 23:34:02 +0000

    create a map to map number to days of week

--- pages/admin/template/[id].tsx
+++ pages/admin/template/[id].tsx
@@ -1,6 +1,20 @@
 import { FC } from "react";
 import { ILessonTemplateWithId } from "../../../model/admin/LessonTemplate.model";
 
+const convertToDayOfWeek: Record<number, string> = {
+  0: "Sunday",
+  1: "Monday",
+  2: "Tuesday",
+  3: "Wednesday",
+  4: "Thursday",
+  5: "Friday",
+  6: "Saturday",
+};
+
+const toTime: Record<string, string> = {
+  "0": "12:00 AM",
+  "0.5": "12:30 AM",
+};
 export const Template: FC<ILessonTemplateWithId> = ({
   id,
   instructor,
@@ -24,6 +38,10 @@ export const Template: FC<ILessonTemplateWithId> = ({
         Start time <span>{startTime}</span>
         End time <span>{endTime}</span>
         Price <span>{price}</span>
+        Location <span>{location}</span>
+        Currency <span>{currency}</span>
+        Availability <span>{availability}</span>
+        Day of the week <span>{convertToDayOfWeek[dayOfWeek]}</span>
       </div>
     </div>
   );
