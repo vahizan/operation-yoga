df56c2d99376b931efc30a64484cb89a814d8897
Vahizan <vahizanv@gmail.com>
2024-04-28 22:52:33 +0100

    user-metadate to receive user admin type

---
+++ pages/api/auth/user-metadata.ts
@@ -0,0 +1,35 @@
+import { getAccessToken } from "@auth0/nextjs-auth0";
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+interface UserMetadata {
+  role: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<UserMetadata | { error: string }>
+) {
+  const { method, query } = req;
+
+  if (method !== "GET") {
+    res.status(404).json({ error: "Method Invalid" });
+    return;
+  }
+
+  try {
+    const { accessToken } = await getAccessToken(req, res);
+    console.log("access token", accessToken);
+    const options = {
+      method: "GET",
+      url: `${process.env.AUTH0_MANAGEMENT_URL}roles/${query?.id}`,
+      headers: { authorization: `Bearer ${accessToken}` },
+    };
+
+    const response = await axios(options);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error("Email sending error:", error);
+    res.status(500).end();
+  }
+}
