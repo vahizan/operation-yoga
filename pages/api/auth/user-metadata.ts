b38ba38491dae87471d69c28d35e1e606976ea85
Vahizan <vahizanv@gmail.com>
2024-05-07 23:41:15 +0100

    aiming to get user roles

--- pages/api/auth/user-metadata.ts
+++ pages/api/auth/user-metadata.ts
@@ -1,4 +1,4 @@
-import { getAccessToken } from "@auth0/nextjs-auth0";
+import { getAccessToken, getSession } from "@auth0/nextjs-auth0";
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
@@ -10,7 +10,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<UserMetadata | { error: string }>
 ) {
-  const { method, query } = req;
+  const { method } = req;
 
   if (method !== "GET") {
     res.status(404).json({ error: "Method Invalid" });
@@ -18,12 +18,16 @@ export default async function handler(
   }
   try {
     const { accessToken } = await getAccessToken(req, res);
-    const options = {
-      method: "GET",
-      url: `${process.env.AUTH0_MANAGEMENT_URL}roles/${query?.id}`,
-      headers: { authorization: `Bearer ${accessToken}` },
-    };
-    const response = await axios(options);
+    const session = await getSession(req, res);
+    const response = await axios.get(
+      `${process.env.AUTH0_MANAGEMENT_URL}/users/${session?.user.sub}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          accept: "application/json",
+        },
+      }
+    );
     res.status(200).json(response.data);
   } catch (error) {
     console.error("Email sending error:", error);
