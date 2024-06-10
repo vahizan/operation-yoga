d4497d29e91ee9716ebe85ab68582d79d2afac85
Vahizan <vahizanv@gmail.com>
2024-06-10 20:12:13 +0100

    create cypress test boilerplates for login and register functionality

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,66 @@
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+const loginFixture = "login";
+describe("Login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("should go to login page", () => {
+    cy.get("a").contains("Login").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/login");
+    });
+  });
+
+  it("should enter email and password incorrectly", () => {
+    cy.get("a").contains("Login").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/login");
+    });
+    cy.get("input[name=email]").type("test");
+    cy.get("input[name=password]").type("test");
+    cy.get("button").contains("Login").click();
+    cy.get("p").contains("Invalid email or password");
+  });
+
+  it("should enter email and password correctly", () => {
+    cy.server();
+    cy.intercept("POST", "/api/login", (req) => {
+        req.reply((res) => {
+            res.send({ fixture: loginFixture });
+        });
+    });
+    cy.get("a").contains("Login").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/login");
+    });
+    cy.get("input[name=email]").type("email@email.com");
+    cy.get("input[name=password]").type("password");
+    cy.get("button").contains("Login").click();
+    cy.get("p").contains("Welcome back");
+  });
+
+  it("should timeout on network error", () => {
+    cy.server();
+    cy.route({
+      method: "POST",
+      url: "/api/login",
+      status: 500,
+      response: {},
+    });
+    cy.get("a").contains("Login").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/login");
+    });
+    cy.get("input[name=email]").type("email@email.com");
+    cy.get("input[name=password]").type("password");
+    cy.get("button").contains("Login").click();
+    cy.get("p").contains("Network error");
+});
+
+// Prevent TypeScript from reading file as legacy script
+export {};
---
+++ cypress/e2e/register.cy.ts
@@ -0,0 +1,70 @@
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+const registerFixture = "register";
+
+describe("Register", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("should go to register page", () => {
+    cy.get("a").contains("Register").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/register");
+    });
+  });
+
+  it("should details incorrectly ", () => {
+    cy.get("a").contains("Register").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/register");
+    });
+    cy.get("input[name=email]").type("test");
+    cy.get("input[name=password]").type("test");
+    cy.get("input[name=confirmPassword]").type("test");
+    cy.get("button").contains("Register").click();
+    cy.get("p").contains("Invalid email or password");
+  });
+
+  it("should enter user details correctly", () => {
+    cy.server();
+    cy.intercept("POST", "/api/register", (req) => {
+        req.reply((res) => {
+            res.send({ fixture: registerFixture });
+        });
+    });
+    cy.get("a").contains("Register").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/register");
+    });
+    cy.get("input[name=email]").type("  ");
+    cy.get("input[name=password]").type("password");
+    cy.get("input[name=confirmPassword]").type("password");
+    cy.get("button").contains("Register").click();
+    cy.get("p").contains("Welcome");
+    });
+
+    it("should timeout on network error", () => {
+        cy.server();
+        cy.route({
+            method: "POST",
+            url: "/api/register",
+            status: 500,
+            response: {},
+        });
+        cy.get("a").contains("Register").click();
+        cy.location().should((loc) => {
+            expect(loc.pathname).to.eq("/register");
+        });
+        cy.get("input[name=email]").type("  ");
+        cy.get("input[name=password]").type("password");
+        cy.get("input[name=confirmPassword]").type("password");
+        cy.get("button").contains("Register").click();
+        cy.get("p").contains("Network error");
+      });
+
+// Prevent TypeScript from reading file as legacy script
+export {};
